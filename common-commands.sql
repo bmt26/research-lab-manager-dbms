c6c718106f37c65e295acc01c1cec9b7c32fd1b7
Brian Temple <bmt26@njit.edu>
2026-05-03 17:52:39 -0400

    File name formatting
    Altered file names to follow a schema and reduced redundant files

---
+++ common-commands.sql
@@ -0,0 +1,3 @@
+# USE ResearchLabManager
+# delete from lab_member where MID > 0
+# DROP TRIGGER insert_lab_member_type;
--- test.sql
+++
@@ -1,2 +0,0 @@
-# delete from lab_member where MID > 0
-# DROP TRIGGER insert_lab_member_type;
